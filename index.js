3d72fd54aae418d6b2b109d8e40e73ec3639097e
Lilia <liliabendjeddou9@gmail.com>
2023-09-08 04:43:29 +0100

    assignment done

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+//  runtime: O(n^2) 
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     const complement = target - array[i];
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] === complement) return true;
+//     }
+//   }
+//   return false;
+// }
+
+
+// Time complexity: O(n)
+// Space complexity: O(n)
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +33,23 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+   if we reach the end of the array, return false
 */
 
 /*
   Add written explanation of your solution here
+
+  make a hasTargetSum function that checks if two numbers in an array add up to some target number. 
+  For example, if the array is [1,2,3,4] and the target number is 6, I know that 2 and 4 add up to 6, 
+  so I should return true. If I have the same array and the target is 10, 
+  no two numbers in the array add up to 10, so I should return false.
 */
 
 // You can run `node index.js` to view these console logs
